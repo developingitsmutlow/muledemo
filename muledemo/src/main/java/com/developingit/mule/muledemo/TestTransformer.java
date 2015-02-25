69cc61dee938c0ea074e5aaceaa2eac926fef792
frantuma@gmail.com <frantuma@ygmail.com>
2015-02-25 15:33:40 +0100

    added test java transformer

---
+++ muledemo/src/main/java/com/developingit/mule/muledemo/TestTransformer.java
@@ -0,0 +1,17 @@
+package com.developingit.mule.muledemo;
+
+import org.mule.api.MuleMessage;
+import org.mule.api.transformer.TransformerException;
+import org.mule.transformer.AbstractMessageTransformer;
+ 
+public class TestTransformer extends AbstractMessageTransformer{
+    /**
+     * @param args
+     */
+    public Object transformMessage(MuleMessage message, String outputEncoding) throws TransformerException {
+            
+    	String payload = message.getPayload(String.class);
+    	payload = payload.replace("order", "arder");
+    	return payload;
+    }
+}
